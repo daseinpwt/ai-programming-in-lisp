633b4ca756d82a8e72ca159bf6651466dfdedbbe
Wentao Pan <daseinpwt@gmail.com>
2019-03-03 21:29:24 +0100

    Add chapter 1

---
+++ src/chapter-1/chapter-1.lisp
@@ -0,0 +1,23 @@
+(defun last-name (name)
+  "Select the last name from a name represented as a list."
+  (first (last name)))
+
+(defun first-name (name)
+  "Select the first name from a name represented as a list."
+  (first name))
+
+(setf names '((John Q Public) (Malcolm X)
+              (Admiral Grace Murray Hopper) (Spot)
+              (Aristotle) (A A Milne) (Z Z Top)
+              (Sir Larry Olivier) (Miss Scarlet)))
+
+(defparameter *titles*
+  '(Mr Mrs Miss Ms Sir Madam Dr Admiral Major General)
+  "A list of titles that can appear at the start of a name.")
+
+(defun first-name_v2 (name)
+  "Select the first name from a name represented as a list.
+   The titles will be filtered"
+  (if (member (first name) *titles*)
+      (first-name_v2 (rest name))
+      (first name)))
