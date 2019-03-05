c5ced283a084fe26a965bf5ae0aa1e6de5d783dd
Wentao Pan <daseinpwt@gmail.com>
2019-03-05 09:40:20 +0100

    Improve example framework

--- src/misc/file-io/load-expression-from-file.lisp
+++ src/misc/file-io/load-expression-from-file.lisp
@@ -1,2 +1,9 @@
+(defun get-file-content (path)
+  "Read the content of a file into one string."
+  (with-open-file (stream path)
+    (let ((contents (make-string (file-length stream))))
+      (read-sequence contents stream)
+      contents)))
+
 (defun load-expr (path)
   (eval (read-from-string (get-file-content path))))
